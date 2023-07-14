62cfdec4053ddf71f30d1dd27b094597cae13e00
rasika1995 <akh.rasika.priyadarshani@gmail.com>
2023-07-14 21:12:22 +0530

    Establish database connection

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express from 'express';
+import sequelize from './db-config/mysql';
 
 const app = express();
-const port = 80;
+const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello, Deliveroo-backend!');
+});
+
+// Test endpoint for checking MySQL connection
+app.get('/test-connection', async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    res.send('MySQL connection successful');
+  } catch (error) {
+    console.error('Error connecting to MySQL:', error);
+    res.status(500).send('MySQL connection failed');
+  }
 });
 
 app.listen(port, () => {
